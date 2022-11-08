ba1fca4e6d56f3dc255beaf6d7f4ba2e87c613fe
19nikilangiu91 <niki.langiu@gmail.com>
2022-11-08 17:28:53 +0100

    document upgrade

--- js/main.js
+++ js/main.js
@@ -1,10 +1,4 @@
-const prezzoKm = 0.21;
 
-// Variabili
-
-let utenteKm, utenteEta, sconto;
-
-sconto = 0;
 
 const nome = document.getElementById("outputone");
 const km = document.getElementById("outputtwo");
@@ -26,8 +20,43 @@ myButton.addEventListener("click",
         nameField.value = null;
         kmField.value = null;
         etaField.value = null;
+
+
+        const prezzoKm = 0.21;
+        
+        // Variabili
+
+        let utenteKm, utenteEta, sconto;
+        
+        sconto = 0;
+
+        if (utenteEta < 18){
+            sconto = 20;
+        } else if (utenteEta >= 65){
+            sconto = 40;
+        }
+
+        console.log(sconto)
+
+        // Calcolare il prezzo totale del biglietto
+
+        const prezzoTotale = prezzoKm * utenteKm;
+
+        console.log(prezzoTotale)
+
+        // Calcolare il prezzo scontanto del biglietto
+
+        const prezzoScontato = prezzoTotale * (100 - sconto) / 100;
+
+        console.log(prezzoScontato)
+
+        // Output
+
+        document.getElementById("title").innerHTML= `Con l'acquisto di questo biglietto,
+        potrà percorrere ${titolokm}Km e avendo ${titoloeta} anni, avrà il diritto di uno
+        sconto del ${sconto}%, il suo prezzo sarà di ${prezzoTotale}€ e il suo prezzo scontato totale sarà di ${prezzoScontato.toFixed(2)}€.`
     }
 )
+        
 
-    
 
